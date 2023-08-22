c82ed728c7e2ff390ec1d9b051544471313681c2
Pavlo Nechai <nechaipavel7@gmail.com>
2023-08-23 00:01:05 +0300

    fix land

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,50 @@ const swiper = new Swiper(".mySwiper", {
         nextEl: '.my-next-button',
     },
 });
+
+// -----------------------
+
+const swiper_rooms = new Swiper(".swiper_rooms", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    initialSlide: 1,
+    spaceBetween: 32,
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.my-button-next',
+    },
+});
+//---------------------------
+
+
+const swiperList = new Swiper('.swiper-list', {
+    slidesPerView: 3, // Визначте, скільки слайдів буде видно одночасно
+    spaceBetween: 32, // Відстань між слайдами
+    navigation: {
+        nextEl: '.custom-next',
+        prevEl: '.custom-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+// ------------------
+
+const photoItems = document.querySelectorAll('.photo-item');
+const orderValues = Array.from(photoItems).map((_, index) => index);
+
+orderValues.sort(() => Math.random() - 0.5);
+
+photoItems.forEach((item, index) => {
+    item.style.setProperty('--order', orderValues[index]);
+});
